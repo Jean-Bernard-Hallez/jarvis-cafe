a3c6c0dc52018d2df1a4799105b1543836126c1c
Hallez <jbhallez@aol.com>
2017-03-15 15:08:57 +0100

    Avec Domoticz, il me prépare à la demande 1 ou 2 tasses de café ou 1 bol. J'ai rajouté le FIBARO System FGS222 dans ma machine philips sensé
    You: Peux tu me préparer une tasse de café ?
    Jarvis:  Est-ce que la tasse de café est bien mise en place ?
    You: Oui
    Jarvis:  Ok je lance la procédure
    
    lorsque c'est fait...
    
    Jarvis:  Votre café est prêt...

--- cafe_1Bol.sh
+++ cafe_1Bol.sh
@@ -5,20 +5,19 @@ Id_Cafe_Bouton2T="841"
 IP_Domoticz="http://192.168.0.17:8080"
 
 sudo -u pi /home/pi/jarvis/jarvis.sh -s "Préchauffage en cours pour le bol de café..."
-/home/pi/jarvis/jarvis.sh -s "Préchauffage en cours..."
-curl "$AddressDomoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_MA&switchcmd=On"
+curl "$IP_Domoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_MA&switchcmd=On"
 sleep 45
 
 echo "J'actionne pour 1 bol"
-curl "$1/json.htm?type=command&param=switchlight&idx=$Id_Cafe_Bouton2T&switchcmd=On"
+curl "$IP_Domoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_Bouton2T&switchcmd=On"
 sleep 22
 
 echo "Attente second préchauffage"
 sleep 22
 
 echo "J'actionne pour encore un peu de café"
-curl "$1/json.htm?type=command&param=switchlight&idx=$Id_Cafe_Bouton2T&switchcmd=On"
+curl "$IP_Domoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_Bouton2T&switchcmd=On"
 sleep 11
 
-curl "$1/json.htm?type=command&param=switchlight&idx=$Id_Cafe_MA&switchcmd=On"
-/home/pi/jarvis/jarvis.sh -s "Votre bol de café est prêt."
+curl "$IP_Domoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_MA&switchcmd=On"
+sudo -u pi /home/pi/jarvis/jarvis.sh -s "Votre bol de café est prêt."
--- fr/functions.sh
+++ fr/functions.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 jv_pg_ct_cafe_1tasse () {
-# jv_curl "$AddressDomoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_1tasse&switchcmd=On"
 /home/pi/jarvis/plugins/jarvis-cafe/cafe_1_tasse.sh
 }
 
 jv_pg_ct_cafe_2tasses () {
-# jv_curl "$AddressDomoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_2tasses&switchcmd=On"
 /home/pi/jarvis/plugins/jarvis-cafe/cafe_2_tasses.sh
 }
 
 jv_pg_ct_cafe_Bol () {
-# jv_curl "$AddressDomoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_Bol&switchcmd=On"
 /home/pi/jarvis/plugins/jarvis-cafe/cafe_1Bol.sh
 }
 
