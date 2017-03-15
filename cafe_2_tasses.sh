f476f5667a1507f92f665f077beb390d15ca6d11
Hallez <jbhallez@aol.com>
2017-03-15 14:42:48 +0100

    Avec Domoticz, il me prépare à la demande 1 ou 2 tasses de café ou 1 bol. J'ai rajouté le FIBARO System FGS222 dans ma machine philips sensé
    You: Peux tu me préparer une tasse de café ?
    Jarvis:  Est-ce que la tasse de café est bien mise en place ?
    You: Oui
    Jarvis:  Ok je lance la procédure
    
    lorsque c'est fait...
    
    Jarvis:  Votre café est prêt...

--- cafe_2_tasses.sh
+++ cafe_2_tasses.sh
@@ -2,7 +2,7 @@
 
 Id_Cafe_MA="843"
 Id_Cafe_Bouton2T="841"
-IP_Domoticz="http://JB:Positif13@192.168.0.17:8080"
+IP_Domoticz="http://192.168.0.17:8080"
 
 sudo -u pi /home/pi/jarvis/jarvis.sh -s "Préchauffage pour deux tasses en cours..."
 curl "$IP_Domoticz/json.htm?type=command&param=switchlight&idx=$Id_Cafe_MA&switchcmd=On"
